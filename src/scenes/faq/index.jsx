67a4db0e064a614c7444124fa27f4cd6ce2350e6
Jeison Novoa <106498984+JeisonNovoa@users.noreply.github.com>
2023-10-03 14:56:30 -0500

    adding FAQ

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -17,65 +17,81 @@ const FAQ = () => {
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            An Important Question
+            What is Ready Player DAO and what is its purpose?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Ready Player DAO is a community governance system designed for
+            projects related to gaming and gaming tokens. Our goal is to empower
+            all community members, allowing them to make meaningful decisions
+            and actively participate in steering the project.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            Another Important Question
+            How can I claim tokens in Ready Player DAO?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            To claim tokens in Ready Player DAO, you first need to connect your
+            wallet and obtain a free NFT. Then, you can visit the token section
+            and follow the instructions to claim your tokens. Please note that
+            in our test environment, tokens are free to facilitate testing.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            Your Favorite Question
+            How can I create a proposal in Ready Player DAO?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            To create a proposal in Ready Player DAO, visit the proposals
+            section and complete the form by providing a detailed description of
+            your proposal, the amount of tokens involved, and the type of
+            proposal (e.g., "maintenance" or "transfer"). Make sure to follow
+            the guidelines, and then submit the proposal for members to vote on.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            Some Random Question
+            How does the voting system work in Ready Player DAO?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            The voting system in Ready Player DAO allows members to vote on
+            proposals created by the community. You can vote for multiple
+            proposals at once or for a single one. Proposals require a minimum
+            number of votes to be valid, and the results are displayed in
+            real-time on our platform.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            The Final Question
+            How can I suggest changes or improvements to Ready Player DAO?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            We are open to suggestions and contributions from the community. If
+            you have ideas for changes or improvements to Ready Player DAO, you
+            can propose them on our governance platform. To do so, simply log in
+            to your account, go to the "Proposals" section, and follow the
+            instructions to create a new proposal. Once the proposal is reviewed
+            and approved by the community, we will work on its implementation.
+            Your active participation is essential to growing our platform and
+            enhancing the experience for all members.
           </Typography>
         </AccordionDetails>
       </Accordion>
